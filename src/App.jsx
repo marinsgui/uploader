9f4fee8512601d4b2449b86fb7c1c078c02f845f
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-02-11 10:54:35 -0300

    upload e download criados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
   
+  const [files, setFiles] = useState([])
+  
+
+  function handleChange(e) {
+    const selected = e.target.files[0]
+    setFiles([...files, selected])
+  }
+
+  function handleDownload(index) {
+    const file = files[index]
+    const url = URL.createObjectURL(file)
+    const link = document.createElement('a')
+    link.href = url
+    link.download = file.name
+    document.body.appendChild(link)
+    link.click()
+  }
 
   return (
     <div className="App">
-      
+      <form>
+        <input type="file" onChange={handleChange} />
+      </form>
+      <div>
+          {files.map((file, index) => (
+            <div key={index}>
+              <div>{file.name}</div>
+              <button onClick={() => handleDownload(index)}>Download</button>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
